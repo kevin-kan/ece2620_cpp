7330f301fab98673e8bb3c8fdc471c9680e4dd1a
kevin-kan <kevinkan939@gmail.com>
2016-10-30 15:47:40 -0400

    basic Lab4

---
+++ labs/lab4/src/main.cc
@@ -0,0 +1,70 @@
+// Kevin Kan
+// ECE 2620:001
+// Lab4: Accessing Dynamic Arrays Through Pointers
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int findmax(int a, int b, int c, int d, int e){
+	int max = a;
+	if (b > max) max = b;
+	if (c > max) max = c;
+	if (d > max) max = d;
+	if (e > max) max = e;
+	return max;
+}
+
+int main(){
+	int numLines, i1, i2, i3, i4, i5;
+
+	/* ASK USER FOR NUMBER OF LINES */
+	int* arrP;
+	cout << "How many lines are in the input file?";
+	cin >>  numLines;
+	//cout << "Number of lines:" << numLines << endl;
+
+	/* DYNAMICALLY CREATE ARRAY */
+	arrP = new int[numLines];
+
+	/* READING FROM FILE */
+	// ifstream infile("lab4_input.txt");
+	ifstream infile;
+	infile.open("lab4_input.txt");
+	if (infile.is_open()){		// Checks if file is really open
+		for (int i = 0; i < numLines; i++){		// Fills array with max #s
+			infile >> i1 >> i2 >> i3 >> i4 >> i5;
+			*(arrP + i) = findmax(i1, i2, i3, i4, i5);
+		}
+		cout << "File Read Complete." << endl;
+		infile.close();
+	}
+	else{
+		cout << "Failed to read from file." << endl;
+		infile.close();
+		return 0;
+	}
+	
+	/* WRITING TO FILE */
+	ofstream outfile;
+	outfile.open("lab4_output.txt");
+	if (outfile.is_open()){
+		for (int i = 0; i < numLines; i++){
+			outfile << *(arrP + i) << endl;
+		}
+		cout << "File Write Complete." << endl;
+		outfile.close();
+	}
+	else{
+		cout << "Failed to write to file." << endl;
+		outfile.close();
+		return 0;
+	}
+
+	/* DELETE DYNAMICALLY CREATED ARRAY*/
+	delete[] arrP;
+	
+	return 0;
+}
+
