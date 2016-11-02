ce83e142302f869ef34eb8b992e285edce6c24e8
kevin-kan <kevinkan939@gmail.com>
2016-11-01 22:46:39 -0400

    Anjuta fixes(?)

---
+++ labs/lab4/src/GenArray.h
@@ -0,0 +1,16 @@
+//Kevin Kan
+//ECE 2620-001
+//Lab4: Generic Array Extra Credit
+
+// ** ABANDONED **
+
+#ifndef _GENARRAY_H_
+#define _GENARRAY_H_
+
+template <typename gen> 
+class GenArray{
+	public:
+		
+};
+
+#endif // _GENARRAY_H_
