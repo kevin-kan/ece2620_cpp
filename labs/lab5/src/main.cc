f17e1a16341668fccbc43a31845d9b85c80b1743
kevin-kan <kevinkan939@gmail.com>
2016-11-16 11:03:49 -0500

    Bubble&Merge working versions

--- labs/lab5/src/main.cc
+++ labs/lab5/src/main.cc
@@ -8,13 +8,14 @@ int main(){
 	ofstream bubbleOut, mergeOut;
 	long signed int arrB[MAXSIZE];
 	long signed int arrM[MAXSIZE];
-	int startT, endT, diffT;
+	double startT, endT, diffT;
 	long int numLines;
 	string line;
 	bool doneReading = false;
 	
 	bool exit = false;
-	int option;
+	int option;	
+	
 	while (!exit){
 		cout << "Hello, what would you like to do?" << endl;
 		cout << "\t (1) Read data from input file" << endl;
@@ -24,29 +25,18 @@ int main(){
 		cin >> option;
 		switch(option){
 			case 1:
-				cout << "Reading from input file: 'lab5_input.txt'..." << endl;
-				/* GET NUMBER OF LINES */
-				infile.open("lab5_input.txt");
-				if (infile.is_open()){		// Checks if file is really open
-					while (getline(infile, line)){
-						numLines++;
-					}
-					cout << "Number of lines:" << numLines << endl;
-					infile.close();
-				}
-				else{
-					cout << "Failed to read from file." << endl;
-					infile.close();
-					return 0;
-				}
-				
+				cout << "Reading from input file: 'lab5_input.txt'..." << endl;				
 				/* READING FROM FILE */
 				infile.open("lab5_input.txt");
-				if (infile.is_open()){			// Checks if file is really open
-					for (int i = 0; i < numLines; i++){
+				if (infile.is_open()){								// Checks if file is really open
+					long int i = 0;
+					while(!infile.eof() && i < MAXSIZE){			// stops at end of file or if MAXSIZE reached
 						infile >> arrB[i];
 						arrM[i] = arrB[i];
+						i++;
 					}
+					numLines = i - 1;
+					cout << "numLines = " << numLines << endl;
 					cout << "File Read Complete." << endl;
 					infile.close();
 					doneReading = true;
@@ -63,14 +53,14 @@ int main(){
 					startT = clock();
 					bubblesort(arrB, numLines);
 					endT = clock();
-					diffT = endT - startT;
+					diffT = (endT - startT)/CLOCKS_PER_SEC;
 					bubbleOut.open("lab5_bubbleout.txt");
 					if (bubbleOut.is_open()){
 						for (int i = 0; i < numLines; i++){
 							bubbleOut << arrB[i] << endl;
 						}
 						cout << "Bubble Sort: File Write Complete." << endl;
-						cout << "Time to sort: " << diffT << " clock cycles." << endl;
+						cout << "Time to sort: " << diffT << " seconds." << endl;
 						bubbleOut.close();
 					}
 					else{
@@ -90,15 +80,15 @@ int main(){
 					startT = clock();
 					mergesort(arrM, numLines);
 					endT = clock();
-					diffT = endT - startT;
+					diffT = (endT - startT)/CLOCKS_PER_SEC;
 					mergeOut.open("lab5_mergeout.txt");
-					cout << "merge outfile open" << endl;
+//					cout << "merge outfile open" << endl;
 					if (mergeOut.is_open()){
 						for (int i = 0; i < numLines; i++){
 							mergeOut << arrM[i] << endl;
 						}
 						cout << "Merge Sort: File Write Complete." << endl;
-						cout << "Time to sort: " << diffT << " clock cycles." << endl;
+						cout << "Time to sort: " << diffT << " seconds." << endl;
 						mergeOut.close();
 					}
 					else{
